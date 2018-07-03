2c10ce6a70e44233289573f46dfe84a2ebdcc0f9
Krunal Astik <krunalastik7@gmail.com>
2018-07-03 20:53:06 +0530

    Directory Structure created

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+	</head>
+	<body>
+		
+	</body>
+</html>
