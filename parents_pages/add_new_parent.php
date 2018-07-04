9cd0349c9786d613c7c453a5b678398fbfb35f74
Jayesh Bhadaniya <bhadaniyajayesh63@gmail.com>
2018-07-04 16:21:19 +0530

    Test commit

---
+++ parents_pages/add_new_parent.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta lang="UTF-8">
+		<link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+		<script type="text/javascript" src="./js/jquery-3.3.1.min.js"></script>
+	</head>
+	<body>
+			<form class="form-horizontal" id="#" action="#" method="post">
+            <div class="form-group" >
+					<label class="control-label col-sm-2 " for="name">Name:</label>
+						<div class="col-sm-10">
+							<input type="text" class=" form-control" id="name" placeholder="Enter your name" name="name"/>
+						</div>
+			  </div> 	
+	</body>
+</html>
